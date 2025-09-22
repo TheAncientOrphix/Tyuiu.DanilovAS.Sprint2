4260aff29bdf0b670d801424629eb0a4f8ed50df
Alexander Danilov <Tyuiu.DanilovAS@outlook.com>
2025-09-22 19:30:14 +0500

    Task2

--- Tyuiu.DanilovAS.Sprint2.Task2.V4.Lib/DataService.cs
+++ Tyuiu.DanilovAS.Sprint2.Task2.V4.Lib/DataService.cs
@@ -11,34 +11,42 @@ namespace Tyuiu.DanilovAS.Sprint2.Task2.V4.Lib
             {
                 result = true;
             }
+
             else if (((x >= 5) && (x <= 9)) && (y >= 5) || (y <= 7))
             {
                 result = true;
             }
+
             else if ((x == 9) && (y >= 3) && (y <= 12))
             {
                 result = true;
             }
+
             else if ((x >= 8) && (x <= 10) && (y >= 8) && (y <= 12))
             {
                 result = true;
             }
+
             else if (x == 10 || x == 11 && (y == 11))
             {
                 result = true;
             }
+
             else if ((y == 11) && (x == 5 || x == 6))
             {
                 result = true;
             }
+
             else if ((x == 4) && (y >=7 && y<=13))
             {
                 result = true;
             }
+
             else if (y == 11 && x == 3)
             {
                 result = true;
             }
+
             else if(x == 3 && y == 7)
             {
                 result = true;
@@ -47,15 +55,18 @@ namespace Tyuiu.DanilovAS.Sprint2.Task2.V4.Lib
             {
                 result = true;
             }
+
             else if ((x == 13) && (y >= 6) && (y <= 8))
             {
                 result = true;
             }
+
             else
             {
                 result = false;
             }
-                return result;
+           
+            return result;
         }
     }
 }

54e72c40aa11078d41d2900008f446cdf554b65e
Alexander Danilov <Tyuiu.DanilovAS@outlook.com>
2025-09-27 20:15:21 +0500

    Task4

--- Tyuiu.DanilovAS.Sprint2.Task4.V12.Lib/DataService.cs
+++ Tyuiu.DanilovAS.Sprint2.Task4.V12.Lib/DataService.cs
@@ -8,9 +8,9 @@ namespace Tyuiu.DanilovAS.Sprint2.Task4.V12.Lib
         public double Calculate(double x, double y)
         {
             double z;
-
-
-            z = Math.Sqrt(x) > y * 2 ? 7 + Math.Pow(7+ 2/(y*y),x) : 3*Math.Pow(x,2)-Math.Pow(Math.Cos(y),2)+10/(y*y - Math.Pow(Math.Sin(x),2)+12) ;
+            z = Math.Sqrt(x) > y * 2
+            ? Math.Pow(7 + 2 / (y * y), x)
+            : (3 * x * x - Math.Pow(Math.Cos(y),2) + 10) / (y * y - Math.Pow(Math.Sin(x),2) + 12);
 
             return Math.Round(z,3);
         }

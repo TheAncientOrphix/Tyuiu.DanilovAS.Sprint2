44248a45585d0d641437b5b2b908cd7cb07f4c40
Alexander Danilov <Tyuiu.DanilovAS@outlook.com>
2025-09-27 23:25:55 +0500

    Task7

---
+++ Tyuiu.DanilovAS.Sprint2.Task7.V12.Test/DataServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.DanilovAS.Sprint2.Task7.V12.Lib;
+namespace Tyuiu.DanilovAS.Sprint2.Task7.V12.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestCheckDotInShadedArea()
+        {
+            DataService ds = new DataService();
+            double x = 0.5;
+            double y = 0.6;
+            var wait = true;
+            var res = ds.CheckDotInShadedArea(x, y);
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void TestTwoCheckDotInShadedArea()
+        {
+            DataService ds = new DataService();
+            double x = 0.5;
+            double y = 2;
+            var wait = false;
+            var res = ds.CheckDotInShadedArea(x, y);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.DanilovAS.Sprint2.Task7.V12/Program.cs
@@ -0,0 +1,49 @@
+﻿using Tyuiu.DanilovAS.Sprint2.Task7.V12.Lib;
+namespace Tyuiu.DanilovAS.Sprint2.Task7.V12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Данилов А.С. | ИИПб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #12                                                             *");
+            Console.WriteLine("* Выполнил: Данилов Александр Сергеевич | ИИПб-25-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу на C#, которая запрашивает исходные данные           *");
+            Console.WriteLine("* (вещественные значения) и вычисляет, находится ли точка с координатами  *");
+            Console.WriteLine("* X,Y в заштрихованной области.                                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write("Введите координату X =>");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите координату Y =>");
+            double y = Convert.ToDouble((Console.ReadLine()));
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            bool result = ds.CheckDotInShadedArea(x, y);
+
+            if (result = true)
+            {
+                Console.Write("Точка находится в заштрихованной области.");
+            }
+            else
+            {
+                Console.Write("Точка не находится в заштрихованной области.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
